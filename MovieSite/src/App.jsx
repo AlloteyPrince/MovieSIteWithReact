c3d5768d3c01eed30a706b08d01fdb28c86b874a
Prince <oscarprince2020@gmail.com>
2025-01-18 17:06:10 -0800

    First Commit

---
+++ MovieSite/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import MovieCard from './Components/MovieCard';
+
+function App() {
+
+  return (
+  <>
+  <MovieCard  movie={{title: "Tims Film", release_date:2024, }}/>
+  </>
+  )
+}
+
+
+
+export default App
